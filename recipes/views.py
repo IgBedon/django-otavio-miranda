c915eb19b628d2cd389600eaa8c3fee98fddaca4
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-10-25 10:53:44 -0300

    Organizing, cleaning and starting HTML (Header -> Probably Home)

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.http.response import HttpResponse
 
 
 def teste(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={'name': 'Bedon'},status=200)
 
