3976b3d40b4205b7933ccc3fabf96a34e1a62eac
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-10-28 14:49:50 -0300

    Inserting data into cards and using conditionals to render

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Bedon'}, status=200)
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+        'is_detail_page': False,
+        }, status=200)
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Bedon'}, status=200)
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+        }, status=200)
 
