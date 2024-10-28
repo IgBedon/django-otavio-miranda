c93a4923e60a7b90fb3d361c94d3cd53653ff010
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-10-28 10:17:48 -0300

    Recipe Page

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 
-def teste(request):
+def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Bedon'}, status=200)
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Bedon'}, status=200)
 
