75b0835da77cc36d8249abb6da2a408df667c762
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-10-30 16:44:28 -0300

    Fixing some lines (only mistakes)

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,11 @@ from utils.recipes.factory import make_recipe
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(10)],
-        'is_detail_page': False,
-        }, status=200)
+        })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
-        }, status=200)
+        })
 
